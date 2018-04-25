6dd353f4b725dc610d59f81978b7c5250b194796
Ichhakhemka <ichha148@gmail.com>
2018-04-25 11:08:27 -0400

    Initial commit

---
+++ filecomparison.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df1 = pd.read_excel('C:\\Users\\ichha\\testdata1.xlsx')
+df2 = pd.read_excel('C:\\Users\\ichha\\testdata2.xlsx')
+
+df1.head()
+
+difference = df2[df1!=df2]
+print difference
